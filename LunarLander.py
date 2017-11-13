6cc23b5a0d26f92faca02da4382798b100d12039
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2017-11-13 20:42:39 +0000

    added the play again option and cleaned up the code

--- LunarLander.py
+++ LunarLander.py
@@ -7,31 +7,44 @@
    and you decide every turn how much fuel to burn which slows down your speed-
    In order to land safely you have to burn the right amount of fuel at the right time-
    Then your description of what the program does.'''
-   
-# setting up the initial variables
-altitude = 1000    #this is the altitude above the moon(distance from the moon in metres)
-velocity = 0       #the speed at which the lunar lander is moving(starts with 0, neither rising nor falling)
-fuel = 1000        #this is the amount of fuel left in litres	
-burn = 0
-OFFSET = 0.15
-GRAVITY = 1.6
 
-while altitude > 0:
-    burn = int(input("Enter fuel to burn: "))
-    if burn < 0:
-        burn = 0
-    if burn > fuel:
-        burn = fuel
-    if burn < fuel:
-        burn = fuel
+def play():
 
-    velocity += GRAVITY
-    velocity -= OFFSET*fuel
-    altitude -= velocity
-    fuel -= burn
-    print('Altitude is:' + int(altitude), 'Velocity is: ' + int(velocity) + 'Fuel is: ' + int(fuel))
+    # setting up the initial variables
+    altitude = 1000    #this is the altitude above the moon(distance from the moon in metres)
+    velocity = 0       #the speed at which the lunar lander is moving(starts with 0, neither rising nor falling)
+    fuel = 1000        #this is the amount of fuel left in litres	
+    burn = 0
+    OFFSET = 0.15
+    GRAVITY = 1.6
 
-if velocity <=10:
-    print('Safe Landing, Well done, YOU WIN!')
-else:
-    print('You blasted into the crater of the MOON! YOU LOSE!')
+    while altitude > 0:
+        burn = int(input("Enter fuel to burn: "))
+        if burn <= 0:
+            burn = 0
+        if burn > fuel:
+            burn = fuel
+
+
+        velocity += GRAVITY
+        velocity -= OFFSET*burn
+        altitude -= velocity
+        fuel -= burn
+        print('Altitude is: ' + str(altitude) + '  Velocity is: ' + str(velocity) + '  Fuel is: ' + str(fuel))
+
+    if velocity <=10:
+        altitude = 0
+        print('Altitude is: ' + str(altitude) + '  Velocity is: ' + str(velocity) + '  Fuel is: ' + str(fuel))
+        print('Safe Landing, Well done, YOU WIN!')
+    else:
+        print('You blasted into the crater of the MOON! YOU LOSE!')
+play()
+while True:
+    answer = input('Would you like to play again? ')
+    if answer == 'Y' or answer == 'y':
+        play()
+    elif answer == 'N' or answer == 'n':
+        print('Thank you for your time, I hope you enjoyed playing this game')
+        break
+    else:
+        print('Please enter Y or y if you would like to play again or /n please enter N or n to stop playing')
